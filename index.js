3aee7b74549031fa31105a0f30af91bf1dcdb4a1
Abhiram <abhikollepara333@gmail.com>
2025-02-19 15:08:15 +0530

    Done

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const { resolve } = require('path');
+const { config } = require('./config');
+
+console.log(config);
 
 const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
+
+
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
